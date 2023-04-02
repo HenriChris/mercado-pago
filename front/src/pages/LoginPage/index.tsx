f66e16698624a533a2ea5f93520ae605e61a1fb8
unset <henrique.chrispim@protonmail.com>
2023-04-02 19:21:50 -0300

    feature: Criação da página de login LoginPage

---
+++ front/src/pages/LoginPage/index.tsx
@@ -0,0 +1,12 @@
+import LoginWrapper from '../../components/LoginWrapper';
+import { Page } from './style';
+import './style.tsx'
+
+export default function LoginPage() {
+  
+    return (
+        <Page>
+            <LoginWrapper/>
+        </Page>
+    );
+};
